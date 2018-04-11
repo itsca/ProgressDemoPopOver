466b2585da6c88f86ec139aa43979081a7986d06
itsca <itscae@gmail.com>
2018-04-11 14:40:09 -0600

    adds normal boostrap progress bar

--- src/components/myProgress/myProgress.jsx
+++ src/components/myProgress/myProgress.jsx
@@ -1,34 +1,67 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './myProgress.css';
+import Icon from '../../assets/images/info-icon.png'
 
 import {
   Overlay, 
   Tooltip, 
   Button,
   Popover,
-  OverlayTrigger
+  OverlayTrigger,
+  ProgressBar
 } from 'react-bootstrap'
+
 
 class MyProgress extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            show: false
+            show: true
         };
     }
   handleMyProgressClicked() {
     console.log('myprogressclicked');
   }
   render() {
+    /*const popoverProgress = (
+        <Popover
+            id="popover-basic"
+            placement="bottom"
+        >
+            50%
+        </Popover>
+      );*/
+      const p = (
+        <ProgressBar now={60} label="60">
+        </ProgressBar>
+      );
+      /*const pp = (
+        <ProgressBar now={1} children={[popoverProgress]} />
+      );*/
+    /*const target = ReactDOM.findDOMNode(p)
+    console.log(target);*/
+    
     const popoverLeft = (
-        <Popover id="popover-positioned-left" title="Popover left">
-          <strong>Holy guacamole!</strong> Check this info.
+        <Popover title="Your Progress">
+          <div className="popOverContainer">
+            <div className="progressContent">
+                <p>Reached:</p>
+                <div class="progress bg-success">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                    
+                    </div>
+                </div>
+            </div>
+            <footer>
+                <p className="targetText" > <img src={Icon} className="infoIcon" alt="info icon"/>You need $69 more to reach your target</p>
+            </footer>
+          </div>
         </Popover>
       );
     return (
       <div>
-        <OverlayTrigger trigger="click" placement="right" overlay={popoverLeft} onEntered={()=>{
+        <OverlayTrigger trigger="click" placement="top" overlay={popoverLeft} onEntered={()=>{
             // Handle progress bar animation
         }}>
             <a href="#">My Progress</a>
