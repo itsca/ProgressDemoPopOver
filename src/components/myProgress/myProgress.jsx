b48c837829bd314cc6c9f67f2b4704e781270fe4
itsca <itscae@gmail.com>
2018-04-11 00:03:54 -0600

    Shows simple Boostrap PopOver with link

--- src/components/myProgress/myProgress.jsx
+++ src/components/myProgress/myProgress.jsx
@@ -1,33 +1,57 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './myProgress.css';
 
 import {
   Overlay, 
   Tooltip, 
-  Button
+  Button,
+  Popover,
+  OverlayTrigger
 } from 'react-bootstrap'
 
 class MyProgress extends Component {
     constructor(props, context) {
         super(props, context);
-
+        this.getTarget = this.getTarget.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
         this.state = {
-            show: true
+            show: false
         };
     }
   handleMyProgressClicked() {
     console.log('myprogressclicked'); 
   }
-  /*getTarget() {
+  getTarget() {
+    console.log(ReactDOM.findDOMNode(this.target));
+    
     return ReactDOM.findDOMNode(this.target);
-  }*/
+  }
+  handleToggle() {
+      console.log('Clicked');
+      
+    this.setState({ show: !this.state.show });
+  }
   render() {
+      console.log(OverlayTrigger);
+      
+    const overlayProps = {
+        container: this,
+        target: this.getTarget,
+        show: true
+    };
+    const popoverLeft = (
+        <Popover id="popover-positioned-left" title="Popover left">
+          <strong>Holy guacamole!</strong> Check this info.
+        </Popover>
+      );
     return (
       <div>
-        <a href="#">My Progress</a>
-        <Overlay show={this.state.show} placement="left">
-          <Tooltip id="overload-left">Tooltip overload!</Tooltip>
-        </Overlay>
+        <OverlayTrigger trigger="click" placement="right" overlay={popoverLeft} onEntered={()=>{
+            // Handle progress bar animation
+        }}>
+            <a href="#">My Progress</a>
+        </OverlayTrigger>
       </div>
     );
   }
